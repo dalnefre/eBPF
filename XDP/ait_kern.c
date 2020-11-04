b012688113279c80d1261b8118998b953ebd6780
Dale Schumacher <dalnefre@yahoo.com>
2020-11-04 16:58:03 -0700

    always use ZERO_COPY strategy

--- XDP/ait_kern.c
+++ XDP/ait_kern.c
@@ -22,7 +22,6 @@ struct bpf_elf_map ait_map SEC("maps") = {
 #include "code.c"  // data encoding/decoding
 
 #define PERMISSIVE 1  // allow non-protocol packets to pass through
-#define ZERO_COPY  1  // apply in-place edits to packet buffer
 #define UNALIGNED  0  // assume unaligned access for packet data
 #define USE_MEMCPY 1  // use __built_in_memcpy() for block copies
 #define LOG_RESULT 0  // log result code for all protocol packets
@@ -36,7 +35,6 @@ struct bpf_elf_map ait_map SEC("maps") = {
 #define __inline  inline __attribute__((always_inline))
 #endif
 
-#if (ZERO_COPY || TRACE_MSG)
 static __inline void
 copy_ait(void *dst, void *src)
 {
@@ -50,7 +48,6 @@ copy_ait(void *dst, void *src)
     *((__u64 *) dst) = *((__u64 *) src);
 #endif
 }
-#endif /* (ZERO_COPY || TRACE_MSG) */
 
 #if TRACE_MSG
 static __inline void
@@ -75,8 +72,6 @@ swap_mac_addrs(void *ethhdr)
     eth[0] = eth[3]; eth[1] = eth[4]; eth[2] = eth[5];
     eth[3] = tmp[0]; eth[4] = tmp[1]; eth[5] = tmp[2];
 }
-
-#if ZERO_COPY
 
 static __inline __u64 *
 acquire_ait()
@@ -180,7 +175,7 @@ handle_message(__u8 *data, __u8 *end)
             }
             case n_5: {  // ack ack
                 if (release_ait(u) < 0) {  // release failed
-                    data[OTHER_OFS] = n_4;
+                    data[OTHER_OFS] = n_4;  // reverse
                 } else {
                     data[OTHER_OFS] = n_6;
 #if LOG_AIT
@@ -257,205 +252,6 @@ handle_message(__u8 *data, __u8 *end)
 #endif
     return XDP_TX;  // send updated frame out on same interface
 }
-
-#else /* !ZERO_COPY */
-
-typedef struct ait {
-    __u64 i;  // outbound
-    __u64 u;  // inbound
-} ait_t;
-
-static __inline int
-next_state(int state)
-{
-    switch (state) {
-    case 0:  return 1;
-    case 1:  return 2;
-    case 2:  return 1;
-    case 3:  return 4;
-    case 4:  return 5;
-    case 5:  return 6;
-    case 6:  return 1;
-    default: return 0;
-    }
-}
-
-#if 0
-static __inline int
-prev_state(int state)
-{
-    switch (state) {
-    case 0:  return 0;
-    case 1:  return 2;
-    case 2:  return 1;
-    case 3:  return 2;
-    case 4:  return 3;
-    case 5:  return 4;
-    case 6:  return 5;
-    default: return 0;
-    }
-}
-#endif
-
-static int
-next_state_ait(int state, ait_t *ait)
-{
-    // conditional state transition (checks for AIT)
-    __u32 key;
-    __u64 *value_ptr;
-
-    int next = next_state(state);
-    ait->u = ait->i;
-
-    key = 0;
-    value_ptr = bpf_map_lookup_elem(&ait_map, &key);
-    if (value_ptr) {
-        if ((state == 1) || (state == 2)) {  // ping/pong
-            __u8 *bp = (void *)value_ptr;
-
-            if (bp[0] != null) {  // AIT ready to send
-                next = 3;
-            }
-        } else if (state == 6) {  // AIT completed
-            bpf_printk("SENT: 0x%llx\n", __builtin_bswap64(*value_ptr));
-            *value_ptr = -1;
-        }
-        ait->i = *value_ptr;
-    }
-
-    key = 1;
-    value_ptr = bpf_map_lookup_elem(&ait_map, &key);
-    if (value_ptr) {
-        if (state == 5) {  // AIT acknowledged
-            bpf_printk("RCVD: 0x%llx\n", __builtin_bswap64(ait->u));
-            *value_ptr = ait->u;
-        } else {
-            ait->u = *value_ptr;
-        }
-    }
-    return next;
-}
-
-static int
-handle_message(__u8 *data, __u8 *end)
-{
-    int offset = ETH_HLEN;
-    int size = 0;
-    int state;
-    int other;
-    int seq_num;
-    ait_t ait = { null, null };
-    int n;
-
-    if (data + offset >= end) return XDP_DROP;  // out of bounds
-    if (data[offset++] != array) return XDP_DROP;  // bad message type
-
-    // get array size (in bytes)
-    size = SMOL2INT(data[offset++]);
-    if ((size < SMOL_MIN) || (size > SMOL_MAX)) {
-        return XDP_DROP;  // bad size
-    }
-//    bpf_printk("array size=%d\n", size);
-    if (data + offset + size > end) return XDP_DROP;  // array to large
-
-    // get `state` field
-    state = SMOL2INT(data[offset++]);
-    if ((state < 0) || (state > 6)) {
-        return XDP_DROP;  // bad state
-    }
-
-    // get `other` field
-    other = SMOL2INT(data[offset++]);
-    if ((other < 0) || (other > 6)) {
-        return XDP_DROP;  // bad other
-    }
-
-//    bpf_printk("state=%d other=%d\n", state, other);
-
-    // get `seq_num` field
-    n = parse_int_n(data + offset, end, &seq_num, 2);
-//    bpf_printk("n=%d seq_num=%d\n", n, seq_num);
-    if (n <= 0) return XDP_DROP;  // parse error
-    offset += n;
-
-    // get `ait` field(s)
-    if (other > 2) {
-//        bpf_printk("data+%d: 0x%x 0x%x\n", offset, data[offset], data[offset+1]);
-        if (data[offset++] != octets) return XDP_DROP;  // require raw bytes
-        if (data[offset++] != n_16) return XDP_DROP;  // require size = 16
-//        bpf_printk("octets n_16 offset=%d\n", offset);
-#if UNALIGNED
-#if USE_MEMCPY
-        __builtin_memcpy(&ait, data + offset, sizeof(ait));
-        offset += sizeof(ait);
-#else
-        ait.i = bytes_to_int64(data + offset);
-        offset += 8;
-        ait.u = bytes_to_int64(data + offset);
-        offset += 8;
-#endif
-#else
-        ait.i = *(__u64 *)(data + offset);
-        offset += 8;
-        ait.u = *(__u64 *)(data + offset);
-        offset += 8;
-#endif
-    }
-
-#if LOG_PROTO
-    bpf_printk("%d,%d #%d <--\n", state, other, seq_num);
-#endif
-
-    // calculate new state
-    state = other;  // swap self <-> other
-    other = next_state_ait(state, &ait);
-    seq_num = (__s16)(seq_num + 1);
-//    bpf_printk("state=%d other=%d seq_num=%d\n", state, other, seq_num);
-
-    // prepare reply message
-    swap_mac_addrs(data);
-    offset = ETH_HLEN;
-    data[offset++] = array;
-    int max_content = ETH_ZLEN - (ETH_HLEN + 2);
-    data[offset++] = INT2SMOL(max_content);  // max array size
-    int content = offset;
-    __builtin_memset((data + content), 0xFF, max_content);
-    data[offset++] = INT2SMOL(state);
-    data[offset++] = INT2SMOL(other);
-    n = code_int_n(data + offset, end, seq_num, 2);
-    if (n <= 0) return XDP_DROP;  // coding error
-    offset += n;
-    if (other > 2) {
-        data[offset++] = octets;  // raw bytes
-        data[offset++] = n_16;  // size = 16
-#if UNALIGNED
-#if USE_MEMCPY
-        __builtin_memcpy(data + offset, &ait, sizeof(ait));
-        offset += sizeof(ait);
-#else
-        int64_to_bytes(data + offset, ait.i);
-        offset += 8;
-        int64_to_bytes(data + offset, ait.u);
-        offset += 8;
-#endif
-#else
-        *(__u64 *)(data + offset) = ait.i;
-        offset += 8;
-        *(__u64 *)(data + offset) = ait.u;
-        offset += 8;
-#endif
-    }
-//    bpf_printk("content=%d offset=%d\n", content, offset);
-    data[content - 1] = INT2SMOL(offset - content);  // final array size
-
-#if LOG_PROTO
-    bpf_printk("%d,%d #%d -->\n", state, other, seq_num);
-#endif
-
-    return XDP_TX;  // send updated frame out on same interface
-}
-
-#endif /* ZERO_COPY */
 
 SEC("prog") int
 xdp_filter(struct xdp_md *ctx)
--- proto/ait.c
+++ proto/ait.c
@@ -14,7 +14,6 @@
 #define DEBUG(x)   /**/
 
 #define SHARED_COUNT 0  // message counter is shared (or local)
-#define ZERO_COPY    1  // apply in-place edits to packet buffer
 #define LOG_RESULT   0  // log result code for all protocol packets
 #define LOG_PROTO    1  // log each protocol messages exchange
 #define LOG_AIT      1  // log each AIT sent/recv
@@ -90,8 +89,6 @@ recv_message(int fd, void *data, size_t limit, struct timespec *ts)
     return n;
 }
 
-#if ZERO_COPY
-
 enum xdp_action {
     XDP_ABORTED = 0,
     XDP_DROP,
@@ -139,11 +136,15 @@ clear_outbound()
 static __inline int
 release_ait(__u64 ait)
 {
+#if 1
+    return 0;  // success!
+#else
     static int once = -1;  // FIXME: fail, but only once...
 
     int rv = once;
     once = 0;
     return rv;
+#endif
 }
 
 static __inline int
@@ -337,6 +338,9 @@ server(int fd)
         perror("send_message() failed");
         return -1;  // failure
     }
+#if LOG_PROTO
+    printf("  %d,%d #%d -->\n", 0, 0, 0);  // init message
+#endif
 
     for (;;) {
 
@@ -360,277 +364,6 @@ server(int fd)
 
     }
 }
-
-#else /* !ZERO_COPY */
-
-typedef struct ait_msg {
-    struct timespec ts;     // timestamp
-    int         state;      // self state
-    int         other;      // other state
-    int         count;      // message count
-    struct {
-        int64_t     i;      // outbound ait
-        int64_t     u;      // inbound ait
-    } ait;
-} ait_msg_t;
-
-static int
-next_state(int state)
-{
-    switch (state) {
-    case 0:  return 1;
-    case 1:  return 2;
-    case 2:  return 1;
-    case 3:  return 4;
-    case 4:  return 5;
-    case 5:  return 6;
-    case 6:  return 1;
-    default: return 0;
-    }
-}
-
-#if 0
-static int
-prev_state(int state)
-{
-    switch (state) {
-    case 0:  return 0;
-    case 1:  return 2;
-    case 2:  return 1;
-    case 3:  return 2;
-    case 4:  return 3;
-    case 5:  return 4;
-    case 6:  return 5;
-    default: return 0;
-    }
-}
-#endif
-
-size_t
-create_message(void *data, size_t size, ait_msg_t *msg)
-{
-    size_t offset = 0;
-    ptrdiff_t n;
-
-    // fill Ethernet frame
-    if (size < ETH_ZLEN) return 0;  // buffer too small
-    memset(data, null, ETH_ZLEN); 
-
-    // add Ethernet header for AF_PACKET SOCK_RAW
-    if (size < ETH_HLEN) return 0;  // buffer too small
-    struct ethhdr *hdr = data;
-    memcpy(hdr->h_dest, eth_remote, ETH_ALEN);
-    memcpy(hdr->h_source, eth_local, ETH_ALEN);
-    hdr->h_proto = htons(proto_opt.eth_proto);
-    offset += ETH_HLEN;
-
-    // payload is a message encoded as an array
-    bstr_t meta = {
-        .base = data,
-        .end = data + offset,
-        .limit = data + size,
-    };
-
-    if (bstr_open_array(&meta) < 0) return 0;
-    if (bstr_put_int(&meta, msg->state) < 0) return 0;
-    if (bstr_put_int(&meta, msg->other) < 0) return 0;
-    if (bstr_put_int16(&meta, msg->count) < 0) return 0;
-    if (msg->other > 2) {
-        if (bstr_put_blob(&meta, &msg->ait, sizeof(msg->ait)) < 0) return 0;
-    }
-    if (bstr_close_array(&meta) < 0) return 0;
-
-    n = (meta.end - meta.start);  // number of bytes written to buffer
-    if (n <= 0) return 0;  // error
-    offset += n;
-
-    return offset;
-}
-
-int
-parse_message(void *data, size_t limit, ait_msg_t *msg_in)
-{
-    size_t offset = 0;
-    int rv;
-
-    // parse Ethernet header
-    offset += ETH_HLEN;
-    if (offset >= limit) return -1;  // out-of-bounds
-    struct ethhdr *hdr = data;
-    if (hdr->h_proto != htons(proto_opt.eth_proto)) return -1;  // bad protocol
-    memcpy(eth_remote, hdr->h_source, ETH_ALEN);  // remember remote MAC
-
-    // parse top-level value
-    bstr_t bstr = {
-        .base = data,
-        .end = data + offset,
-        .limit = data + limit,
-    };
-    json_t json = {
-        .bstr = &bstr,
-    };
-    rv = json_get_value(&json);
-    if (rv <= 0) return -1;  // error
-    if (json.type != JSON_Array) return -1;  // error
-
-    // parse array elements
-    bstr_t part = {
-        .end = bstr.cursor,
-        .limit = bstr.end,
-    };
-    json_t item = {
-        .bstr = &part,
-    };
-    part.end = bstr.cursor;
-    part.limit = bstr.end;
-//    while (part.end < part.limit) {
-//        rv = json_get_value(&item);
-//        if (rv <= 0) return -1;  // error
-//        ...
-//    }
-
-    // read "state" from message
-    rv = json_get_int64(&item);
-    if (rv <= 0) return -1;  // error
-    msg_in->state = item.val.num.bits;
-
-    // read "change" from message
-    rv = json_get_int64(&item);
-    if (rv <= 0) return -1;  // error
-    msg_in->other = item.val.num.bits;
-
-    // read "count" from message
-    rv = json_get_int64(&item);
-    if (rv <= 0) return -1;  // error
-    msg_in->count = item.val.num.bits;
-
-    // read "ait" from message
-    if (msg_in->other > 2) {
-        rv = json_get_value(&item);
-        if (rv <= 0) return -1;  // error
-        if (item.type != JSON_String) return -1;  // require String
-        rv = sizeof(msg_in->ait);
-        if (item.count != rv) return -1;  // require size = 16
-        memcpy(&msg_in->ait, part.cursor, rv);
-    } else {
-        msg_in->ait.i = -1;
-        msg_in->ait.u = -1;
-    }
-
-    offset = bstr.end - bstr.base;  // update final offset
-    return offset;
-}
-
-int
-process_message(ait_msg_t *in, ait_msg_t *out)
-{
-    // deliver ait
-    if (in->other == 5) {
-        fputs("AIT rcvd:\n", stdout);
-        hexdump(stdout, &in->ait.i, sizeof(in->ait.i));
-    }
-
-    // act on message received
-    out->state = in->other;
-    out->other = next_state(out->state);
-    out->ait.u = in->ait.i;
-    if (proto_opt.ait) {
-        size_t z = sizeof(out->ait.i);
-        switch (out->state) {
-            case 1:  // FALL-THRU
-            case 2:
-                strncpy((char *)&out->ait.i, proto_opt.ait, z);
-                out->other = 3;
-                fputs("AIT send:\n", stdout);
-                hexdump(stdout, &out->ait.i, sizeof(out->ait.i));
-                break;
-            case 6:
-                if (strlen(proto_opt.ait) < z) {
-                    proto_opt.ait = NULL;
-                } else {
-                    proto_opt.ait += z;
-                }
-                break;
-        }
-    } else {
-        out->ait.i = -1;
-    }
-#if SHARED_COUNT
-    out->count = in->count + 1;  // update message counter
-#else
-    out->count = out->count + 1;  // update message counter
-#endif
-
-#if LOG_PROTO
-#if DUMP_PACKETS
-    printf("  %d,%d #%d -> %d,%d #%d\n",
-        in->state, in->other, in->count,
-        out->state, out->other, out->count);
-#else
-    printf("  %zu.%09ld %d,%d #%d -> %d,%d #%d\n",
-        (size_t)in->ts.tv_sec, (long)in->ts.tv_nsec,
-        in->state, in->other, in->count,
-        out->state, out->other, out->count);
-#endif
-#endif
-
-#if PACKET_LIMIT
-    if (out->count > PACKET_LIMIT) return 0;  // FIXME: halt ping/pong!
-#endif
-
-    return 1;  // continue...
-}
-
-int
-server(int fd)
-{
-    int rv = 1;
-    ait_msg_t msg_out = {
-        .state = 0,
-        .other = 0,
-        .count = 0,
-        .ait.i = null,
-        .ait.u = null,
-    };
-    ait_msg_t msg_in;
-
-    while (rv > 0) {
-
-        rv = create_message(proto_buf, sizeof(proto_buf), &msg_out);
-        if (rv <= 0) {
-            fprintf(stderr, "error encoding message in %zu-byte buffer\n",
-                sizeof(proto_buf));
-            break;  // failure
-        }
-
-//        rv = send_message(fd, proto_buf, n);
-        rv = send_message(fd, proto_buf, ETH_ZLEN, &msg_out.ts);
-        if (rv <= 0) {
-            perror("send_message() failed");
-            break;  // failure
-        }
-
-        rv = recv_message(fd, proto_buf, sizeof(proto_buf), &msg_in.ts);
-        if (rv <= 0) {
-            perror("recv_message() failed");
-            break;  // failure
-        }
-
-        rv = parse_message(proto_buf, rv, &msg_in);
-        if (rv <= 0) {
-            perror("parse_message() failed");
-            break;  // failure
-        }
-
-        // process message
-        rv = process_message(&msg_in, &msg_out);
-
-    }
-
-    return rv;
-}
-
-#endif /* ZERO_COPY */
 
 int
 main(int argc, char *argv[])
