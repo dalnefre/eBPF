1de56fca969e0633051fbb5825f5062ddad9829f
Dale Schumacher <dalnefre@yahoo.com>
2020-12-03 19:06:41 -0700

    auto refresh toggle

--- http/html/link_demo.js
+++ http/html/link_demo.js
@@ -1,10 +1,40 @@
 $(function () {
+    let $inbond = $('#inbond');
     let $outbound = $('#outbound');
+    let $raw_data = $('#raw_data');
 
-    $('#send').click(function (e) {
+    var waiting = false;  // waiting for server response
+    let refresh = function () {
+        if (waiting) return;  // prevent overlapping requests
+        waiting = true;
         $.getJSON('/ebpf_map/ait.json')
             .done(function (data) {
-                $outbound.text(JSON.stringify(data, null, 2));
+                $raw_data.text(JSON.stringify(data, null, 2));
+                waiting = false;
             });
+    };
+
+    var interval = false;
+    let startRefresh = function (delay) {
+        delay = delay || 1000;  // default update rate = 1 per second
+        refresh();
+        interval = setInterval(refresh, delay);
+    };
+    let stopRefresh = function () {
+        clearInterval(interval);
+        interval = false;
+    };
+    let toggleRefresh = function () {
+        if (interval) {
+            stopRefresh();
+        } else {
+            startRefresh();
+        }
+    };
+
+    $('#send').click(function (e) {
+        toggleRefresh();
     });
+
+    startRefresh();
 });
