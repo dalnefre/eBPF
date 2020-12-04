f61305c314fd86a5f691fc087786ecb072208c5c
Dale Schumacher <dalnefre@yahoo.com>
2020-12-03 20:07:16 -0700

    packet count & overlap warning

--- http/html/link_demo.js
+++ http/html/link_demo.js
@@ -1,14 +1,20 @@
 $(function () {
     let $inbond = $('#inbond');
+    let $pkt_count = $('#pkt_count');
     let $outbound = $('#outbound');
     let $raw_data = $('#raw_data');
 
     var waiting = false;  // waiting for server response
     let refresh = function () {
-        if (waiting) return;  // prevent overlapping requests
+        if (waiting) {
+            $raw_data.addClass("error");
+            return;  // prevent overlapping requests
+        }
         waiting = true;
+        $raw_data.removeClass("error");
         $.getJSON('/ebpf_map/ait.json')
             .done(function (data) {
+                $pkt_count.val(data.ait_map[3].n);
                 $raw_data.text(JSON.stringify(data, null, 2));
                 waiting = false;
             });
