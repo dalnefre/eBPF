f276144f15edaa4f73574ee2c2b99e9933806751
Dale Schumacher <dale.schumacher@gmail.com>
2020-12-17 11:38:58 -0700

    add rebuild steps

--- link_demo.sh
+++ link_demo.sh
@@ -1,12 +1,18 @@
 #
-# on reboot, use this script to set up the link demo configuration
+# on reboot, use this script to rebuild and run the link demo configuration
 #
 
+cd proto
+make
+cd ..
+
 cd XDP
+make
 sudo ip -force link set dev eth0 xdp obj ait_kern.o
 cd ..
 
 cd http
+make && sudo make install
 sudo cgi-fcgi -start -connect /run/ebpf_map.sock ./ebpf_fcgi
 sudo chown www-data /run/ebpf_map.sock
 cd ..
