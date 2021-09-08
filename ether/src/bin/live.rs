8c311384a93fa032cb491e8dc31432c28ca58c1d
Dale Schumacher <dalnefre@yahoo.com>
2021-09-08 13:55:51 -0600

    add link::Link object

--- ether/src/bin/live.rs
+++ ether/src/bin/live.rs
@@ -1,4 +1,3 @@
-use pretty_hex::pretty_hex;
 use std::env;
 
 mod wire {
@@ -6,7 +5,6 @@ mod wire {
         self, Channel::Ethernet, DataLinkReceiver, DataLinkSender, NetworkInterface,
     };
     use pretty_hex::pretty_hex;
-    use rand::Rng;
 
     pub struct Wire {
         tx: Box<dyn DataLinkSender>,
@@ -39,24 +37,16 @@ mod wire {
             Wire { tx, rx }
         }
 
-        pub fn send_init_frame(&mut self) {
+        pub fn send_init_frame(&mut self, nonce: u32) {
             // Construct and send a single packet.
-            let frame = b"\
-                \xFF\xFF\xFF\xFF\xFF\xFF\
-                \x00\x00\x00\x00\x00\x00\
+            let header = b"\
+                \x7F\xFF\xFF\xFF\xFF\xFF\
+                \x7F\x00\x00\x00\x00\x00\
                 \x88\xB5\
-                \xF0\xF0\
-                \x20\x20\x20\x20\x20\x20\x20\x20\
-                \x20\x20\x20\x20\x20\x20\x20\x20\
-                \x20\x20\x20\x20\x20\x20\x20\x20\
-                \x20\x20\x20\x20\x20\x20\x20\x20\
-                \x20\x20\x20\x20\x20\x20\x20\x20\
-                \x20\x20\x20\x20";
+                \x00\x00";
             let mut buffer = [0x20_u8; 60];
-            buffer.copy_from_slice(frame);
-            let nonce: u32 = rand::thread_rng().gen();
-            buffer[8..12].copy_from_slice(&nonce.to_le_bytes());
-            buffer[2..6].copy_from_slice(&0x12345678_u32.to_be_bytes());
+            buffer[0..16].copy_from_slice(header);
+            buffer[8..12].copy_from_slice(&nonce.to_be_bytes());
             println!("SEND_INIT {}", pretty_hex(&buffer));
             self.tx.send_to(&buffer, None);
         }
@@ -68,9 +58,59 @@ mod wire {
 }
 use wire::Wire;
 
-pub fn on_frame_recv(frame: &[u8]) {
-    println!("FRAME_RECV {}", pretty_hex(&frame));
+mod link {
+    use crate::wire::Wire;
+    use pretty_hex::pretty_hex;
+    use rand::Rng;
+
+    pub struct Link {
+        wire: Wire,
+        nonce: u32,
+    }
+
+    impl Link {
+        pub fn new(wire: Wire) -> Link {
+            let nonce: u32 = rand::thread_rng().gen();
+            Link { wire, nonce }
+        }
+
+        pub fn send_init(&mut self) {
+            self.wire.send_init_frame(self.nonce);
+        }
+
+        pub fn event_loop(&mut self) {
+            loop {
+                println!("LOOP");
+                match self.wire.recv_frame() {
+                    Ok(frame) => {
+                        self.on_recv(&frame);
+                    }
+                    Err(e) => {
+                        // If an error occurs, we can handle it here
+                        panic!("An error occurred while reading: {}", e);
+                    }
+                }
+            }
+        }
+
+        pub fn on_recv(&mut self, frame: &[u8]) {
+            println!("LINK_RECV {}", pretty_hex(&frame));
+            let proto = frame[6];
+            if proto == 0x7F {  // init/reset protocol
+                println!("init/reset protocol {}", proto);
+                let mut tree_id = [0; 4];
+                tree_id.copy_from_slice(&frame[8..12]);
+                let other = u32::from_be_bytes(tree_id);
+                println!("nonce {}, other {}", self.nonce, other);
+            } else if proto == 0xFF {  // entangled protocol
+                println!("entangled protocol {}", proto);
+            } else {
+                panic!("Unexpected protocol {}", proto);
+            }
+        }
+    }
 }
+use link::Link;
 
 // Implement link liveness protocol
 //
@@ -84,24 +124,12 @@ fn main() {
         }
         Some(name) => name,
     };
-    let mut wire = Wire::new(&interface_name);
+    let wire = Wire::new(&interface_name);
+    let mut link = Link::new(wire);
 
     // Start protocol
-    wire.send_init_frame();
+    link.send_init();
 
     // Listen loop...
-    loop {
-        println!("LOOP");
-        match wire.recv_frame() {
-            Ok(frame) => {
-                on_frame_recv(&frame);
-            }
-            Err(e) => {
-                // If an error occurs, we can handle it here
-                panic!("An error occurred while reading: {}", e);
-            }
-        }
-    }
-
-    //    println!("EXIT");
+    link.event_loop();
 }
