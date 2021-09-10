863046aa13143e9712209c20a3a628bb45bad654
Dale Schumacher <dalnefre@yahoo.com>
2021-09-10 16:46:23 -0600

    inbound and outbound

--- ether/src/bin/ait.rs
+++ ether/src/bin/ait.rs
@@ -10,12 +10,16 @@ fn async_io() {
     println!("AIT");
 
     // Create a simple streaming channel
-    let (tx, rx) = channel::<&[u8]>();
+    let (inbound_tx, inbound_rx) = channel::<&[u8]>();
+    let (outbound_tx, outbound_rx) = channel::<&[u8]>();
     thread::spawn(move || {
-        let msg = "foobar".as_bytes();
-        tx.send(msg).unwrap();
+        let outbound_msg = "bar".as_bytes();
+        outbound_tx.send(outbound_msg).unwrap();
+        println!("inbound: {:?}", inbound_rx.recv().unwrap());
     });
-    println!("recv: {:?}", rx.recv().unwrap());
+    println!("outbound: {:?}", outbound_rx.recv().unwrap());
+    let inbound_msg = "foo".as_bytes();
+    inbound_tx.send(inbound_msg).unwrap();
 }
 
 fn liveness(if_name: &str) {
