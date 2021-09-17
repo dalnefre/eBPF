d90476b57ea50db6a9a3599ca039c9d6835c594d
Dale Schumacher <dalnefre@yahoo.com>
2021-09-16 21:25:42 -0600

    bridge Ethernet to Actors

--- ether/src/bin/ait.rs
+++ ether/src/bin/ait.rs
@@ -1,10 +1,18 @@
+use std::convert::TryInto;
 use std::env;
-use std::sync::mpsc::channel;
 use std::thread;
+use crossbeam::crossbeam_channel::unbounded as channel;
+use crossbeam::crossbeam_channel::{Sender, Receiver};
 
-//use ether::reactor::*;
-use ether::link::Link;
-use ether::wire::Wire;
+use ether::reactor::{Behavior, Config, Effect, Error, Event, Message};
+use pnet::datalink::{self, Channel::Ethernet, NetworkInterface};
+use rand::Rng;
+extern crate alloc;
+//use alloc::boxed::Box;
+use alloc::rc::Rc;
+
+use ether::link::{Link, LinkBeh};
+use ether::wire::{Wire, WireBeh};
 
 fn async_io() {
     println!("AIT");
@@ -23,6 +31,112 @@ fn async_io() {
 }
 
 fn liveness(if_name: &str) {
+    let if_names_match = |iface: &NetworkInterface| iface.name == if_name;
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .filter(if_names_match)
+        .next()
+        .expect(&format!("Bad interface {}", if_name));
+    // Create a new channel, dealing with layer 2 packets
+    let (mut ether_tx, mut ether_rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}",
+            e
+        ),
+    };
+
+    let (inbound_tx, inbound_rx) = channel::<[u8; 60]>();
+    let (outbound_tx, outbound_rx) = channel::<[u8; 60]>();
+
+    thread::spawn(move || {
+        loop {
+            match ether_rx.next() {
+                Ok(raw_data) => {
+                    let data = raw_data.try_into().expect("Bad frame size");
+                    inbound_tx.send(data).expect("Send failed on channel");
+                }
+                Err(e) => {
+                    // If an error occurs, we can handle it here
+                    panic!("Ethernet read failed: {}", e);
+                }
+            }
+        }
+    });
+
+    thread::spawn(move || {
+        loop {
+            match outbound_rx.recv() {
+                Ok(data) => {
+                    ether_tx.send_to(&data, None);
+                }
+                Err(e) => {
+                    // If an error occurs, we can handle it here
+                    panic!("Channel read failed: {}", e);
+                }
+            }
+        }
+    });
+
+    thread::spawn(move || {
+        struct Boot {
+            tx: Sender<[u8; 60]>,
+            rx: Receiver<[u8; 60]>,
+        }
+        impl Boot {
+            pub fn new(tx: Sender<[u8; 60]>, rx: Receiver<[u8; 60]>) -> Box<dyn Behavior> {
+                Box::new(Boot { tx, rx })
+            }
+        }
+        impl Behavior for Boot {
+            fn react(&self, _event: Event) -> Result<Effect, Error> {
+                let mut effect = Effect::new();
+
+                let wire = effect.create(WireBoot::new(self.tx.clone(), self.rx.clone()));
+                let nonce = rand::thread_rng().gen();
+                let link = effect.create(LinkBeh::new(&wire, nonce));
+                effect.send(&wire, Message::Addr(Rc::clone(&link)));
+
+                Ok(effect)
+            }
+        }
+        struct WireBoot {
+            tx: Sender<[u8; 60]>,
+            rx: Receiver<[u8; 60]>,
+        }
+        impl WireBoot {
+            pub fn new(tx: Sender<[u8; 60]>, rx: Receiver<[u8; 60]>) -> Box<dyn Behavior> {
+                Box::new(WireBoot { tx, rx })
+            }
+        }
+        impl Behavior for WireBoot {
+            fn react(&self, event: Event) -> Result<Effect, Error> {
+                let mut effect = Effect::new();
+                match event.message {
+                    Message::Addr(link) => {
+                        effect.update(WireBeh::new(&link, self.tx.clone(), self.rx.clone()))?;
+                        effect.send(&event.target, Message::Empty);  // start polling
+                        Ok(effect)
+                    }
+                    _ => Ok(effect),
+                }
+            }
+        }
+
+        let mut config = Config::new();
+        config.boot(Boot::new(outbound_tx, inbound_rx));
+        loop {
+            let _pending = config.dispatch(100);
+        }
+    });
+}
+
+fn _liveness(if_name: &str) {
+    // this version is just a refactor of `live.rs`
+    // to be removed once the actor-based version works
     println!("AIT");
 
     let wire = Wire::new(&if_name);
--- ether/src/wire.rs
+++ ether/src/wire.rs
@@ -45,7 +45,7 @@ M/N: { 0 = Global, 1 = Local Admin }
 
 use pnet::datalink::{self, Channel::Ethernet, DataLinkReceiver, DataLinkSender, NetworkInterface};
 use pretty_hex::pretty_hex;
-use std::io::{Error, ErrorKind};
+//use std::io::{Error, ErrorKind};
 
 pub struct Wire {
     tx: Box<dyn DataLinkSender>,
@@ -113,9 +113,63 @@ impl Wire {
     pub fn recv_frame(&mut self) -> Result<&[u8], Error> {
         self.cnt += 1;
         if self.cnt > 5 {
-            Err(Error::new(ErrorKind::Other, "Recv limit reached"))
+            //Err(Error::new(ErrorKind::Other, "Recv limit reached"))
+            Err("Recv limit reached")
         } else {
-            self.rx.next()
+            self.rx.next().map_err(|_| "Recv failed")
+        }
+    }
+}
+
+use crate::reactor::{Actor, Behavior, Message, Event, Effect, Error};
+extern crate alloc;
+//use alloc::boxed::Box;
+use alloc::rc::Rc;
+use crossbeam::crossbeam_channel::{Sender, Receiver};
+
+pub struct WireBeh {
+    link: Rc<Actor>,
+    tx: Sender<[u8; 60]>,
+    rx: Receiver<[u8; 60]>,
+}
+
+impl WireBeh {
+    pub fn new(link: &Rc<Actor>, tx: Sender<[u8; 60]>, rx: Receiver<[u8; 60]>) -> Box<dyn Behavior> {
+        Box::new(WireBeh {
+            link: Rc::clone(&link),
+            tx,
+            rx,
+        })
+    }
+}
+
+impl Behavior for WireBeh {
+    fn react(&self, event: Event) -> Result<Effect, Error> {
+        let mut effect = Effect::new();
+        match event.message {
+            Message::Frame(data) => {
+                match self.tx.send(data) {
+                    Ok(_) => Ok(effect),
+                    _ => Err("send failed")
+                }
+            }
+            Message::Empty => {
+                // FIXME: this polling strategy is only needed
+                // until we can inject events directly into ReActor
+                match self.rx.try_recv() {
+                    Ok(data) => {
+                        effect.send(&self.link, Message::Frame(data));
+                        Ok(effect)
+                    },
+                    _ => {
+                        // FIXME: we should actually check for errors
+                        // _OTHER THAN_ not data available
+                        effect.send(&event.target, Message::Empty);  // keep polling
+                        Ok(effect)
+                    }
+                }
+            }
+            _ => Err("unknown message"),
         }
     }
 }
